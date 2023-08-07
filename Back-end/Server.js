e30bddf70344765693d48a9b8f2257c741269f20
jayb25 <91479113+jayb25@users.noreply.github.com>
2023-08-07 19:30:28 +0930

    Changes made to server file

--- Back-end/Server.js
+++ Back-end/Server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { chats } = require("./data/data");
+const res = require("express/lib/response");
 
 const app = express();
 
@@ -11,4 +12,7 @@ app.get("/api/chat", (req, res) => {
   res.send(chats);
 });
 
+app.get("/api/chat/:id", (req, res) => {
+  console.log(req);
+});
 app.listen(5000, console.log("Server started on port 5000"));
